6d0b707df5fe8134d40b96d779c9b61c43633fcd
Thinkman Wang <thinkman_wang@azhuoinfo.com>
2016-05-28 23:09:38 +0800

    update

--- app/src/main/java/com/umeng/soexample/App.java
+++ app/src/main/java/com/umeng/soexample/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     {
         //寰俊    wx12342956d1cab4f9,a5ae111de7d9ea137e88a5e02c07c94d
-        PlatformConfig.setWeixin("wxad8f4f480c2310bb", "cd35785ddef1c803424c89372836e655");
+        PlatformConfig.setWeixin("wxad8f4f480c2310bb", "7d206eef60694c94e7b572cf56b84094");
         //璞嗙摚RENREN骞冲彴鐩墠鍙兘鍦ㄦ湇鍔″櫒绔厤缃�
         //鏂版氮寰崥
         PlatformConfig.setSinaWeibo("3921700954", "04b48b094faeb16683c32669824ebdad");
